3f99d37487f6af1c7c0e0754ec3f314a58397e03
Rudie Dirkx <github@hotblocks.nl>
2025-01-24 00:54:07 +0100

    +UnusedVariablesRule

---
+++ src/Php/UnusedVariablesNodeVisitor.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace rdx\PhpstanExtra\Php;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\ArrayDimFetch;
+use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\StaticPropertyFetch;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\StaticVar;
+use PhpParser\Node\Stmt\For_;
+use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Global_;
+use PhpParser\Node\Stmt\While_;
+use PhpParser\NodeVisitor;
+use PhpParser\NodeVisitorAbstract;
+
+final class UnusedVariablesNodeVisitor extends NodeVisitorAbstract {
+
+	public const DEFINE = 'define';
+	public const USE = 'use';
+	public const USE_ALL = 'use_all';
+
+	private int $depth = 0;
+	private int $loopDepth = 0;
+	/** @var list<Expr> */
+	private array $skipVars = [];
+	private int $line;
+	/** @var list<array{string, string, int}> */
+	private array $encounters = [];
+	/** @var list<string> */
+	private array $ignoreVarNames = [];
+
+	public function __construct(
+		private bool $debug,
+	) {}
+
+	public function enterNode(Node $node) {
+		$this->depth++;
+		$this->line = $node->getStartLine();
+
+		// echo "\t\t - ", get_class($node), " (",  $this->line, ") - \n";
+
+		if (in_array($node, $this->skipVars)) {
+			return;
+		}
+
+		// Don't go into FunctionLike because they are their own scope
+		if ($node instanceof FunctionLike) {
+			// Except ArrowFunction, that doesn't have its own scope
+			if ($node instanceof ArrowFunction) {
+				return;
+			}
+
+			if ($node instanceof Closure) {
+				foreach ($node->uses as $useVar) {
+					if (is_string($useVar->var->name)) {
+						$varName = $useVar->var->name;
+						$this->addEncounter(self::USE, $varName);
+					}
+				}
+			}
+
+			return NodeVisitor::DONT_TRAVERSE_CHILDREN;
+		}
+
+		if ($node instanceof FuncCall && $node->name instanceof Name) {
+// dump($node->name, $node->name->toLowerString());
+			if ($node->name->toLowerString() === 'compact') {
+				$varNames = $this->getCompactVarNames($node);
+				foreach ($varNames as $varName) {
+					$this->addEncounter(self::USE, $varName);
+				}
+			}
+
+			if ($node->name->toLowerString() === 'get_defined_vars') {
+				$this->addEncounter(self::USE_ALL, 'all');
+			}
+		}
+
+		if ($node instanceof StaticVar) {
+			if ($varName = $this->getVarName($node->var)) {
+				$this->ignoreVarNames[] = $varName;
+			}
+		}
+
+		if ($node instanceof Global_) {
+			foreach ($node->vars as $var) {
+				if ($varName = $this->getVarName($var)) {
+					$this->ignoreVarNames[] = $varName;
+				}
+			}
+		}
+
+		if ($node instanceof For_ || $node instanceof While_) {
+			$this->loopDepth++;
+		}
+
+		if ($node instanceof Foreach_) {
+			// Because of loopDepth, add the source var explicitly
+			$varName = $this->getVarName($node->expr);
+			$this->addEncounter(self::USE, $varName);
+
+			$this->loopDepth++;
+
+			$this->skipVars[] = $node->keyVar;
+			$this->skipVars[] = $node->valueVar;
+			return;
+		}
+
+		if ($node instanceof Assign) {
+			$this->skipVars[] = $node->var;
+			return;
+		}
+
+		if ($node instanceof Variable) {
+			$varName = $this->getVarName($node);
+			$this->addEncounter(self::USE, $varName);
+		}
+	}
+
+	public function leaveNode(Node $node) {
+		$this->depth--;
+		$this->line = $node->getStartLine();
+
+		if ($node instanceof Assign) {
+			$this->skipVars[] = $node->var;
+			$varName = $this->getVarName($node->var);
+			$this->addEncounter(self::DEFINE, $varName);
+			return;
+		}
+
+		if ($node instanceof For_ || $node instanceof While_) {
+			$this->loopDepth--;
+		}
+
+		if ($node instanceof Foreach_) {
+			$this->loopDepth--;
+		}
+	}
+
+	public function getEncounters() : array {
+		return $this->encounters;
+	}
+
+	public function getIgnoreVarNames() : array {
+		return $this->ignoreVarNames;
+	}
+
+	public function getEncounterDebugs() : array {
+		return array_map(function(array $encounter) {
+			[$type, $varName, $line] = $encounter;
+			return sprintf('%s $%s (%d)', $type, $varName, $line);
+		}, $this->encounters);
+	}
+
+	private function addEncounter(string $type, ?string $varName) : void {
+		if (!$varName) return;
+
+		// echo "      $type \$$varName (",  $this->line, ")\n";
+
+		if ($this->loopDepth > 0 && $type == self::DEFINE) return;
+
+		$this->encounters[] = [$type, $varName, $this->line];
+	}
+
+	private function getCompactVarNames(FuncCall $node) : array {
+		$varNames = [];
+		foreach ($node->args as $arg) {
+			if ($arg instanceof Arg && $arg->value instanceof String_) {
+				$varNames[] = $arg->value->value;
+			}
+		}
+		return $varNames;
+	}
+
+	private function getVarName(Expr $node) : ?string {
+		if ($node instanceof Variable) {
+			if (is_string($node->name)) {
+				return $node->name;
+			}
+
+			if ($this->debug) dump(__LINE__, $node);
+			return null;
+		}
+
+		if ($node instanceof PropertyFetch) {
+			return null;
+		}
+
+		if ($node instanceof StaticPropertyFetch) {
+			return null;
+		}
+
+		if ($node instanceof ArrayDimFetch) {
+			// Doesn't understand object ArrayAccess is okay, because doesn't know $node->var type.
+			// return $this->getVarName($node->var);
+			return null;
+		}
+
+		if ($this->debug) dump(__LINE__, $node);
+
+		return null;
+	}
+
+}
---
+++ src/Php/UnusedVariablesRule.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace rdx\PhpstanExtra\Php;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\FunctionLike;
+use PhpParser\NodeTraverser;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+// Alternative: https://github.com/Slamdunk/phpstan-extensions/blob/v1.2.0/lib/UnusedVariableRule.php
+
+final class UnusedVariablesRule implements Rule {
+
+	private const DEBUG = false;
+
+	private const IGNORE_VARS = [
+		'_SERVER',
+		'_REQUEST',
+		'_POST',
+		'_GET',
+	];
+
+	public function __construct(
+		protected bool $enabled,
+	) {}
+
+	public function getNodeType() : string {
+		return FunctionLike::class;
+	}
+
+	public function processNode(Node $node, Scope $scope) : array {
+		// ArrowFunction is not its own scope
+		if ($node instanceof ArrowFunction) {
+			return [];
+		}
+
+		if (self::DEBUG) {
+			printf("\tLine % 4d  %s\n", $node->getStartLine(), get_class($node));
+// if (!in_array($node->getStartLine(), [191])) return [];
+		}
+
+		$ignoreVarNames = [];
+		if ($node instanceof Closure) {
+			foreach ($node->uses as $var) {
+				if ($var->byRef && is_string($var->var->name)) {
+					$ignoreVarNames[] = $var->var->name;
+				}
+			}
+		}
+		foreach ($node->getParams() as $var) {
+			if ($var->byRef && is_string($var->var->name)) {
+				$ignoreVarNames[] = $var->var->name;
+			}
+		}
+
+		$traverser = new NodeTraverser();
+		$traverser->addVisitor($visitor = new UnusedVariablesNodeVisitor(self::DEBUG));
+		$traverser->traverse($node->getStmts() ?? []);
+
+		$ignoreVarNames = array_merge($ignoreVarNames, $visitor->getIgnoreVarNames());
+
+		if (self::DEBUG) {
+			echo "\t\t" . implode("\n\t\t", $visitor->getEncounterDebugs()), "\n";
+		}
+
+		$unusedVariables = [];
+		foreach ($visitor->getEncounters() as [$type, $varName, $line]) {
+			if (in_array($varName, $ignoreVarNames)) {
+				continue;
+			}
+
+			if ($type == UnusedVariablesNodeVisitor::DEFINE) {
+				$unusedVariables[$varName] ??= $line;
+			}
+
+			if ($type == UnusedVariablesNodeVisitor::USE) {
+				unset($unusedVariables[$varName]);
+			}
+
+			if ($type == UnusedVariablesNodeVisitor::USE_ALL) {
+				$unusedVariables = [];
+			}
+		}
+
+		$messages = [];
+		foreach ($unusedVariables as $varName => $line) {
+			if (in_array($varName, self::IGNORE_VARS)) continue;
+
+			$messages[] = RuleErrorBuilder::message(sprintf('Variable $%s seems to be unused.', $varName))
+				->line($line)
+				->identifier('rudie.UnusedVariablesRule')
+				->build();
+		}
+
+		return $messages;
+	}
+
+}
